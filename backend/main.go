cc3e36dda4c7efdf605e194eb50e9b0c5cdf4cda
Pau Ortiz <2202058@ucc.edu.ar>
2024-05-21 12:03:15 -0300

    Estructuras (Cursos, Inscripciones, Usuario)

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,1 @@
 package main
-
-import (
-	"final/db"
-	"log"
-	"net/http"
-)
-
-func main() {
-	db.GetDB()
-
-	log.Println("Server is running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
