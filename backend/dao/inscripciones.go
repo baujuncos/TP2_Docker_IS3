d554e50d18ca79047220912398fd70cebaa1a95e
Pau Ortiz <2202058@ucc.edu.ar>
2024-06-02 17:51:03 -0300

    Se creo BD

--- backend/dao/inscripciones.go
+++ backend/dao/inscripciones.go
@@ -8,7 +8,6 @@ type Inscripcion struct {
 	IdUsuario        int       `gorm:"column:Id_usuario"`
 	IdCurso          int       `gorm:"column:Id_curso"`
 	FechaInscripcion time.Time `gorm:"column:fecha_inscripcion"`
-	Estado           string    `gorm:"column:estado"`
 
 	Usuario User  `gorm:"foreignKey:IdUsuario;references:IdUsuario"`
 	Curso   Curso `gorm:"foreignKey:IdCurso;references:IdCurso"`
