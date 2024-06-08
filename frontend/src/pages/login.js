91ded16c6eb58269cbea22b91c8d344f774b32a5
baujuncos <baujuncos@gmail.com>
2024-06-07 22:52:17 -0300

    cookies implementadas en login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,20 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// Login.js
+import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
 
-function Login() {
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [cookies, setCookie] = useCookies(['token']);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:8080/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            });
+
+            if (!response.ok) {
+                throw new Error('Login failed');
+            }
+
+            const data = await response.json();
+            const { id, token } = data;
+
+            setCookie('token', token, { path: '/' });
+
+            // Redirect or perform another action upon successful login
+            window.location.href = 'http://localhost:3000/'; // example redirection
+
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Login failed. Please check your credentials and try again.');
+        }
+    };
+
     return (
         <div className="login-container">
-            <h2>LOGIN</h2>
-            <p>Nombre de Usuario</p>
-            <input className="caja-nombre-usuario" type="text" placeholder="Ingresa tu nombre de usuario"/>
-            <p>Contraseña</p>
-            <input className="caja-contraseña" type="password" placeholder="Ingresa tu contraseña"/>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
             <div>
-                <button type="button" className="login-button">Login</button>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
             </div>
+            <div>
+                <label htmlFor="password">Contraseña:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </div>
+            <button type="submit">Login</button>
+        </form>
         </div>
-
     );
-}
+};
 
 export default Login;
