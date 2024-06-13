aa2f98b199708e46990bf2a30e6bbed0406d70c3
Pau Ortiz <2202058@ucc.edu.ar>
2024-06-12 21:48:43 -0300

    .

--- frontend/src/components/ComponenteColumna.js
+++ frontend/src/components/ComponenteColumna.js
@@ -5,8 +5,6 @@ export const ComponenteColumna = () => {
     return (
         <div className="componente-columna">
             <p><Link className="home-texto" to="/">Home</Link></p>
-            <p><Link className="cursos-texto" to="/cursos">Cursos</Link></p>
-            <p><Link className="inscripcion-texto" to="/inscripcion">Inscripción</Link></p>
             <p><Link className="miscursos-texto" to="/miscursos">Mis Cursos</Link></p>
         </div>
     )
